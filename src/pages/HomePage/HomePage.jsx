036786d6b60983cfd8217546e4e918401b980a37
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-04-05 15:32:23 +0300

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,11 @@
 import css from '../HomePage/HomePage.module.css';
-
+import image from '../../img/image.png';
 export default function Home() {
   return (
     <div className={css.container}>
       <h1 className={css.title}>Create your Phonebook</h1>
 
-      <img
-        className={css.img}
-        src="src/img/image.png"
-        alt="Image Contact Page"
-      />
+      <img className={css.img} src={image} alt="Image Contact Page" />
     </div>
   );
 }
